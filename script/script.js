6000292635c569e00e2b3907b34e4ac84f7ea21c
NeilSerr97 <neilserrano97@gmail.com>
2021-02-07 23:15:40 -0500

    Actividad 7 Bootcamp

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function enviarformulario() {
+
+    alert("Enviado con Exito, espera pronto tu respuesta")
+
+}
+
+function reserva() {
+
+    alert("Reserva enviada, espera tu confirmación en el correo")
+}
